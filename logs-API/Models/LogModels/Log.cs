b1f2d0803605f217e4325578e978b062091c71d5
Sascha Stropek <sascha.stropek@gmail.com>
2022-07-30 23:24:13 +0200

    Added userJourneys and logs interface

---
+++ logs-API/Models/LogModels/Log.cs
@@ -0,0 +1,11 @@
+﻿namespace logs_API.Models.Logs
+{
+    public class Log
+    {
+        public string Id { get; set; } = string.Empty;
+        public int Timestamp { get; set; } = 0;
+        public string Type { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+    }
+}
---
+++ logs-API/Models/LogModels/UserJourney.cs
@@ -0,0 +1,9 @@
+﻿namespace logs_API.Models.Logs
+{
+    public class UserJourney
+    {
+        public string Id { get; set; } = string.Empty;
+        public int Timestamp { get; set; } = 0;
+        public Log[] Logs { get; set; } = Array.Empty<Log>();
+    }
+}
