030db52da670a38426159e41f0a28af4d63acd9e
Sascha Stropek <sascha.stropek@gmail.com>
2022-07-30 22:02:33 +0200

    Projektdateien hinzufügen.

---
+++ logs-API/Models/Log.cs
@@ -0,0 +1,10 @@
+﻿namespace logs_API.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public int Timestamp { get; set; }
+        public string Type { get; set; } = "";
+        
+    }
+}
