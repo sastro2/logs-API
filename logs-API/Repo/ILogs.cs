ef7403e2f28918a592ece060ca11234526e8d6f7
Sascha Stropek <sascha.stropek@gmail.com>
2022-07-30 23:19:37 +0200

    Added userJourneys and logs interface

---
+++ logs-API/Repo/ILogs.cs
@@ -0,0 +1,12 @@
+﻿using logs_API.Models.Logs;
+
+namespace logs_API.Repo
+{
+    public interface ILogs
+    {
+        public IEnumerable<Log> GetLogs();
+        public Log GetLog(string id);
+        public void CreateLogs(UserJourney userJourney);
+        public void DeleteLog(string id);
+    }
+}
---
+++ logs-API/Repo/InMemLogRepo.cs
@@ -0,0 +1,27 @@
+﻿using logs_API.Models.Logs;
+
+namespace logs_API.Repo
+{
+    public class InMemLogRepo : ILogs
+    {
+        public void CreateLogs(UserJourney userJourney)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteLog(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Log GetLog(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Log> GetLogs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
